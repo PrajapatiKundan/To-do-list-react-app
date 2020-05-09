39dfa17d059d6651409c6efe3a16eb4b67a5f0fd
Kundan0-prajapati <170390107034@saffrony.ac.in>
2020-05-09 19:46:01 +0530

    Updates

---
+++ precache-manifest.c2ed32817533cb2699ffa7ab5254fcb5.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d533ee29d7529471834104025d0f2ab2",
+    "url": "/To-do-list-react-app/index.html"
+  },
+  {
+    "revision": "ac437a6f811f9af06c5d",
+    "url": "/To-do-list-react-app/static/js/2.1a16c7a9.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/To-do-list-react-app/static/js/2.1a16c7a9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f86b939dcb197f4ecb63",
+    "url": "/To-do-list-react-app/static/js/main.11af9f1c.chunk.js"
+  },
+  {
+    "revision": "7860b321a2980f17f979",
+    "url": "/To-do-list-react-app/static/js/runtime-main.43d4a73c.js"
+  }
+]);
